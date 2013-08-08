5a2ad4824f9a31718f53b32ff9a8066b5c893a23
theylive <theylive@hush.com>
2013-08-08 07:45:14 -0400

    foobar 0.0.3
    preparing for launch

--- config.js
+++ config.js
@@ -1,45 +1,44 @@
 /* foobar is a creation of theylive, de nada */
 
 //TURNTABLE CREDENTIALS
-global.auth = "FzPiMNPjGnBjVoriKQzAFYYp";
-global.botid = "51958aa5eb35c1598caf8627";
-global.room = "51fc9246eb35c104c8d2ffd7";
-//global.room = "51df63e5eb35c114fa61a584";
+global.auth = "";
+global.botid = "";
+global.room = "";
 
 //TWITTER CREDENTIALS
-global.consumer_key = "J15bwPB24CCSEhloPAQ";
-global.consumer_secret = "F7jSTgIyHFC9DjCxjdgIDXkzWOirrdZ2HbLz4PmGQag";
-global.access_token = "1557173010-6QphsxZ6uZDcJRmAtkUZJCNeFuh8h01q0LqPzcr";
-global.access_token_secret = "1H90EhcQhetNUcWBwnPc3Kn3vp0yaMQ0Csk2hU6Q";
-global.tweet_trend = "#YOLO";
+global.consumer_key = "";
+global.consumer_secret = "";
+global.access_token = "";
+global.access_token_secret = "";
+global.tweet_trend = "";
 global.tweet_mention = "";
 
 //BOT PROFILE SETTINGS
-global.botname = "@swagbot";
-global.bot_avatar = "18";
-global.bot_laptop = "linux";
-global.bot_twitter = "@f0obarbaz";
+global.botname = "";
+global.bot_avatar = "";
+global.bot_laptop = "";
+global.bot_twitter = "";
 global.bot_soundcloud = ""; 
 global.bot_website = "";
 global.bot_facebook = ""; 
-global.bot_about = "you only live once";
-global.bot_favArtists = "theylive, my daddy";
-global.bot_hangout = "YOLO.FM";
+global.bot_about = "";
+global.bot_favArtists = "";
+global.bot_hangout = "";
 
 //MODE SETTINGS, (true=on, false=off)
 global.afk_mode = false;
 global.queue_mode = false;
-global.auto_queue = true;
+global.auto_queue = false;
 global.chat_mode = false;
 global.battle_mode = false;
 global.autodj_mode = false;
 global.solo_mode = false;
-global.announce_mode = true;
+global.announce_mode = false;
 global.tweet_mode = false;
-global.game_mode = true;
-global.stat_boot = true;
+global.game_mode = false;
+global.stat_boot = false;
 global.event_mode = false;
-global.troll_cop = true;
+global.troll_cop = false;
 
 //MISC. TIMERS
 global.afklimit = 10;//in minutes 
@@ -48,33 +47,33 @@ global.queue_wait_time = 30;//in seconds
 global.event_djs = 3;
 
 //ROOM INFO
-global.botgreet = "welcome, YOLO";//greeting to users on entrance
-global.modgreet = "welcome back God!";//greeting to mods on entrance
-global.ffa_msg = "it's ffa, keep it classy as always";//message for free for all mode
-global.queue_msg = "the queue is on, 1 play per dj, /add to get in line";//message for queue mode
-global.battle_msg = "dj battle is on 1v1, 1 song, +1 at last 30 seconds of song to vote. winner stays up, /add to get in line";//message for battle mode
-global.boot_msg = "le pew";//message for a reason when booting users
-global.event_warn = "decks reserved for event";//message during event mode when djs hop up out of turn
-global.battle_warn = "decks reserved for battle";//message during battle mode when djs hop up out of turn
-global.autodjup_msg = "i'll spin fur a bit";//message for when the bot will auto dj
-global.dive_msg = "/dive";//message for users who stagedive
-global.slow_queue_msg = "/me is gonna turn off ze queue";//message to turn the auto queue off
-global.start_queue_msg = "/me is gonna start up ze queue";//message to turn the auto queue on
-global.autodjdown_msg_now = "/dive";//message to say when the bot is not longer needed right now for auto dj
-global.autodjdown_msg_later = "i\'ll hop down after this spin";//message to say when bot is djing but no longer needed in autodj
-global.theme = "yolo, swag";//the default theme of the room
+global.botgreet = "";//greeting to users on entrance
+global.modgreet = "";//greeting to mods on entrance
+global.ffa_msg = "";//message for free for all mode
+global.queue_msg = "";//message for queue mode
+global.battle_msg = "";//message for battle mode
+global.boot_msg = "";//message for a reason when booting users
+global.event_warn = "";//message during event mode when djs hop up out of turn
+global.battle_warn = "";//message during battle mode when djs hop up out of turn
+global.autodjup_msg = "";//message for when the bot will auto dj
+global.dive_msg = "";//message for users who stagedive
+global.slow_queue_msg = "";//message to turn the auto queue off
+global.start_queue_msg = "";//message to turn the auto queue on
+global.autodjdown_msg_now = "";//message to say when the bot is not longer needed right now for auto dj
+global.autodjdown_msg_later = "";//message to say when bot is djing but no longer needed in autodj
+global.theme = "";//the default theme of the room
 
 //enter your rules, as many lines as you need
 global.rules = [ 
-	"☞ Stay SWAG",
-	"☞ keep it YOLO"
+	"",
+	""
 ];
 
 //congrats message for djs' thousand point parties
 global.congrats_msgs = [
-	"awwwwwwwwwwwwwwwww YE!",
-	"YOLO",
-	"#SWAGGIN"
+	"",
+	"",
+	""
 ];
 
 //banned artists (type artist names, enter as many as needed)
@@ -86,7 +85,6 @@ global.banned_artists = [
 
 //blacklisted userids (just add as many userids as you need to, they are banned upon entrance)
 global.banlist = [
-    //"51e4ddf6aaa5cd222d94232b",//pizaboy alt
     ""
 ];
 
@@ -107,7 +105,7 @@ global.memes = [
 
 //nuke launch codes (there must be three)
 global.launchcodes = [
-    "foo",
-	"bar",
-	"baz"
+    "",
+	"",
+	""
 ];
