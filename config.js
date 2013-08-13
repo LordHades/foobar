608d6f8bb38df999a7eee5527e5f3dff875a9ad9
theylive <theylive@hush.com>
2013-08-13 10:42:14 -0400

    foobar 0.5.x
    major command refactor

--- config.js
+++ config.js
@@ -1,83 +1,97 @@
 /* foobar is a creation of theylive, de nada */
 
-//TURNTABLE CREDENTIALS
-global.master = "512314d8eb35c149eb832448";
-global.auth = "FzPiMNPjGnBjVoriKQzAFYYp";
-global.botid = "51958aa5eb35c1598caf8627";
-global.room = "51fc9246eb35c104c8d2ffd7";
+/**************************************************/
+/*            TURNTABLE CREDENTIALS               */
+/**************************************************/
+global.master = "xxx";
+global.auth = "xxx";
+global.botid = "xxx";
+global.room = "xxx";
 
-//TWITTER CREDENTIALS
-global.consumer_key = "J15bwPB24CCSEhloPAQ";
-global.consumer_secret = "F7jSTgIyHFC9DjCxjdgIDXkzWOirrdZ2HbLz4PmGQag";
-global.access_token = "1557173010-6QphsxZ6uZDcJRmAtkUZJCNeFuh8h01q0LqPzcr";
-global.access_token_secret = "1H90EhcQhetNUcWBwnPc3Kn3vp0yaMQ0Csk2hU6Q";
-global.tweet_trend = "#YOLO";
-global.tweet_mention = "";
+/**************************************************/
+/*            TWITTER CREDENTIALS                 */
+/**************************************************/
+global.consumer_key = "xxx";
+global.consumer_secret = "xxx";
+global.access_token = "xxx";
+global.access_token_secret = "xxx";
+global.tweet_trend = "xxx";
+global.tweet_mention = "xxx";
 
-//BOT PROFILE SETTINGS
-global.botname = "@swagbot";
-global.bot_avatar = "18";
-global.bot_laptop = "linux";
-global.bot_twitter = "@f0obarbaz";
+/**************************************************/
+/*           BOT PROFILE SETTINGS                 */
+/**************************************************/
+global.botname = "xxx";
+global.bot_avatar = "xxx";
+global.bot_laptop = "xxx";
+global.bot_twitter = "xxx";
 global.bot_soundcloud = ""; 
-global.bot_website = "";
-global.bot_facebook = ""; 
-global.bot_about = "you only live once";
-global.bot_favArtists = "theylive, my daddy";
+global.bot_website = "http://theylive.github.io/foobar/";
+global.bot_facebook = "xxx"; 
+global.bot_about = "xxx";
+global.bot_favArtists = "xxx";
 global.bot_hangout = "YOLO.FM";
 
-//MODE SETTINGS, (true=on, false=off)
-global.afk_mode = true;
+/**************************************************/
+/*           MODE DEFUALT SETTINGS                */
+/**************************************************/
+global.afk_mode = false;
 global.queue_mode = false;
-global.auto_queue = true;
-global.chat_mode = true;
+global.auto_queue = false;
+global.chat_mode = false;
 global.battle_mode = false;
 global.autodj_mode = false;
 global.solo_mode = false;
 global.announce_mode = false;
 global.tweet_mode = false;
-global.game_mode = true;
-global.stat_boot = true;
+global.game_mode = false;
+global.stat_boot = false;
 global.event_mode = false;
-global.troll_cop = true;
+global.troll_cop = false;
 
-//MISC. TIMERS
+/**************************************************/
+/*                MISC. TIMERS                    */
+/**************************************************/
 global.afklimit = 10;//in minutes 
 global.afk_dj_limit = 15;//in seconds
 global.queue_wait_time = 30;//in seconds
 global.event_djs = 3;
 
-//ROOM INFO
-global.botgreet = "welcome, YOLO";
-global.modgreet = "welcome back God!";
-global.ffa_msg = "it's ffa, keep it classy as always";
-global.queue_msg = "the queue is on, 1 play per dj, /add to get in line";
-global.battle_msg = "dj battle is on 1v1, 1 song, +1 at last 30 seconds of song to vote. winner stays up, /add to get in line";
-global.boot_msg = "le pew";
-global.event_warn = "decks reserved for event";
-global.battle_warn = "decks reserved for battle";
-global.autodjup_msg = "i'll spin fur a bit";
-global.dive_msg = "/dive";
-global.slow_queue_msg = "/me is gonna turn off ze queue";
-global.start_queue_msg = "/me is gonna start up ze queue";
-global.autodjdown_msg_now = "/dive";
-global.autodjdown_msg_later = "i'll hop down after this spin";
-global.theme = "yolo, swag";
+/**************************************************/
+/*          ROOM INFO, EVENT MESSAGES             */
+/**************************************************/
+global.botgreet = "xxx";
+global.modgreet = "xxx";
+global.ffa_msg = "xxx";
+global.queue_msg = "xxx";
+global.battle_msg = "xxx";
+global.boot_msg = "xxx";
+global.event_warn = "xxx";
+global.battle_warn = "xxx";
+global.autodjup_msg = "xxx";
+global.dive_msg = "xxx";
+global.slow_queue_msg = "xxx";
+global.start_queue_msg = "xxx";
+global.autodjdown_msg_now = "xxx";
+global.autodjdown_msg_later = "xxx";
+global.theme = "xxx";
 
 //enter your rules, as many lines as you need
 global.rules = [ 
-	"☞ Stay SWAG",
-	"☞ keep it YOLO"
+	"xxx",
+	"xxx"
 ];
 
 //congrats message for djs' thousand point parties
 global.congrats_msgs = [
-	"awwwwwwwwwwwwwwwww YE!",
-	"YOLO",
-	"#SWAGGIN"
+	"xxx",
+	"xxx",
+	"xxx"
 ];
 
-//banned artists (type artist names, enter as many as needed)
+/**************************************************/
+/*             BANNED ARTIST LIST                 */
+/**************************************************/
 global.banned_artists = [
     "Katy Perry",
     "Justin Bieber",
@@ -90,17 +104,21 @@ global.banned_artists = [
     "Hilary Duff",
     "Paris Hilton",
     "Heidi Montag",
-    "Selena Gomez"
+    "Selena Gomez",
+    "Brittney Spears"
 ];
 
-//blacklisted userids (just add as many userids as you need to, they are banned upon entrance)
+/**************************************************/
+/*              BANNED USERS LIST                 */
+/**************************************************/
 global.banlist = [
-    "51e4ddf6aaa5cd222d94232b",//pizzaboy521 (monsters...)
-    "50621263eb35c1237200003d",//ryan james phelan
+    "",
     ""
 ];
 
-//memes to send to djs, and others...
+/**************************************************/
+/*                  MEMES LIST                    */
+/**************************************************/
 global.memes = [
     "http://goo.gl/0fh1c", 
 	"http://goo.gl/LcYUb",
@@ -115,9 +133,11 @@ global.memes = [
 	"http://goo.gl/uMUZ9"
 ];
 
-//nuke launch codes (there must be three)
+/**************************************************/
+/*        NUKE LAUNCHCODES, MUST BE 3             */
+/**************************************************/
 global.launchcodes = [
-    "foo",
-	"bar",
-	"baz"
+    "xxx",
+	"xxx",
+	"xxx"
 ];
